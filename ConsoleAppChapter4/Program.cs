c92a0dfb8d152a7326a9674a276c5e5fa99c75bd
jaramirez85 <jaramirez85@gmail.com>
2020-07-20 14:37:45 -0500

    Listing 4.18 Creating a Word document and saving it using C# 4

--- ConsoleAppChapter4/Program.cs
+++ ConsoleAppChapter4/Program.cs
@@ -12,29 +12,16 @@ namespace ConsoleAppChapter4
     {
         static void Main(string[] args)
         {
-            /*
-              A lot of code is required just to create and save a document, including 20 occurrences 
-            of ref missing. It’s hard to see the useful part of the code within the forest of arguments 
-            you don’t care about.
-             */
-
-            object missing = Type.Missing;
 
             Application app = new Application { Visible = true };
-            Document doc = app.Documents.Add(ref missing, ref missing, ref missing, ref missing);
-            Paragraph para = doc.Paragraphs.Add(ref missing);
-            para.Range.Text = "Awkward old code";
+            Document doc = app.Documents.Add();
+            Paragraph para = doc.Paragraphs.Add();
+            para.Range.Text = "Simple new code";
 
-            object fileName = @"C:\Users\javie\source\repos\ConsoleAppChapter4\MyFiles\demo1.docx";
-            doc.SaveAs2(ref fileName, ref missing,
-                ref missing, ref missing, ref missing,
-                ref missing, ref missing, ref missing,
-                ref missing, ref missing, ref missing,
-                ref missing, ref missing, ref missing,
-                ref missing, ref missing);
+            doc.SaveAs2(FileName: "demo2.docx");
 
-            doc.Close(ref missing, ref missing, ref missing);
-            app.Application.Quit(ref missing, ref missing, ref missing);
+            doc.Close();
+            app.Application.Quit();
 
             Console.ReadKey();
 

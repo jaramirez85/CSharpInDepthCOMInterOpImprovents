d86159be11ab2e878c178fc492d068f03404fe59
jaramirez85 <jaramirez85@gmail.com>
2020-07-20 14:07:09 -0500

    First Commit - Listing 4.16 Setting a range of values in Excel with implicit dynamic conversion

---
+++ ConsoleAppChapter4/Program.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.Office.Interop.Excel;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppChapter4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            var app = new Application { Visible = true };
+            app.Workbooks.Add();
+            Worksheet sheet = app.ActiveSheet;
+            Range start = sheet.Cells[1, 1];
+            Range end = sheet.Cells[1, 20];
+            sheet.Range[start, end].Value = Enumerable.Range(1, 20).ToArray();
+
+            Console.ReadKey();
+
+        }
+    }
+}

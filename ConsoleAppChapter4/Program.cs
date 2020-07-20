0ac3c57771686626f9c60f6182e00da25bab945e
jaramirez85 <jaramirez85@gmail.com>
2020-07-20 14:24:55 -0500

    Listing 4.17 Creating a Word document and saving it before C# 4

--- ConsoleAppChapter4/Program.cs
+++ ConsoleAppChapter4/Program.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Office.Interop.Excel;
+﻿
+using Microsoft.Office.Interop.Word;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,13 +12,29 @@ namespace ConsoleAppChapter4
     {
         static void Main(string[] args)
         {
+            /*
+              A lot of code is required just to create and save a document, including 20 occurrences 
+            of ref missing. It’s hard to see the useful part of the code within the forest of arguments 
+            you don’t care about.
+             */
 
-            var app = new Application { Visible = true };
-            app.Workbooks.Add();
-            Worksheet sheet = app.ActiveSheet;
-            Range start = sheet.Cells[1, 1];
-            Range end = sheet.Cells[1, 20];
-            sheet.Range[start, end].Value = Enumerable.Range(1, 20).ToArray();
+            object missing = Type.Missing;
+
+            Application app = new Application { Visible = true };
+            Document doc = app.Documents.Add(ref missing, ref missing, ref missing, ref missing);
+            Paragraph para = doc.Paragraphs.Add(ref missing);
+            para.Range.Text = "Awkward old code";
+
+            object fileName = @"C:\Users\javie\source\repos\ConsoleAppChapter4\MyFiles\demo1.docx";
+            doc.SaveAs2(ref fileName, ref missing,
+                ref missing, ref missing, ref missing,
+                ref missing, ref missing, ref missing,
+                ref missing, ref missing, ref missing,
+                ref missing, ref missing, ref missing,
+                ref missing, ref missing);
+
+            doc.Close(ref missing, ref missing, ref missing);
+            app.Application.Quit(ref missing, ref missing, ref missing);
 
             Console.ReadKey();
 
